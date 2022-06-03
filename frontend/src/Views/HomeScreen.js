b1ac7fe4e922258272d4e9d83830a8674667103e
md-ashikul-islam <md.ashikul.islam@g.bracu.ac.bd>
2022-06-03 16:33:25 +0600

    Added backend api for products

--- frontend/src/Views/HomeScreen.js
+++ frontend/src/Views/HomeScreen.js
@@ -1,12 +1,22 @@
-import data from '../data';
+// import data from '../data'; its a Static data in front end, we want to switch to backend api calls for data
 import { Link } from 'react-router-dom';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 
 function HomeScreen() {
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await axios.get('/api/products');
+      setProducts(result.data);
+    };
+    fetchData();
+  }, []);
   return (
     <div>
       <h1>Featured Products</h1>
       <div className="products">
-        {data.products.map((product) => (
+        {products.map((product) => (
           <div className="product" key={product.slug}>
             <Link to={`/products/${product.slug}`}>
               <img src={product.image} alt={product.name}></img>
