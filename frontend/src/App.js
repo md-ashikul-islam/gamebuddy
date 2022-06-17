fa48194c4c5317bf78758678e472fd439f158f25
md-ashikul-islam <md.ashikul.islam@g.bracu.ac.bd>
2022-06-17 18:51:26 +0600

    Add Product Screen Done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -20,7 +20,7 @@ function App() {
           </Navbar>
         </header>
         <main>
-          <Container>
+          <Container className="mt-3">
             <Routes>
               <Route path="/products/:slug" element={<ProductScreen />} />
               <Route path="/" element={<HomeScreen />} />
